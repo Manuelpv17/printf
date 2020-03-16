305029131565b985958e2f8694ba2fbefb8eee88
Tomas Gomez <tomasgvg.t@gmail.com>
2020-03-16 20:11:02 +0000

    added print_reverse function

---
+++ print_reverse.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_reverse - Prints strings.
+ * @_reverse:----
+ * Return: number of characters printed.
+ */
+
+int print_reverse(va_list _reverse)
+{
+	char *s = va_arg(_reverse, char *);
+	int i, j, number;
+
+	number = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+		number++;
+	}
+	return (number);
+}
